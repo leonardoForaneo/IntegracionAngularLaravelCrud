afc62e888ad7c1a3cb530f3623f2268e3a9e0e23
leonardoForaneo <leonardomartinez.contact@gmail.com>
2021-07-08 19:59:27 -0500

    Parte de laravel para la integracion creada y probada

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -11,20 +11,11 @@ class MovieController extends Controller
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
-     */
+     */ //(GET)
     public function index()
     {
-        echo 'HEllo';
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $movies = Movie::get();
+        echo json_encode($movies);
     }
 
     /**
@@ -32,33 +23,21 @@ class MovieController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
-     */
+     */ //Store lo que hace por regla es crear un nuevo elemento con los parametros
+    //Guarda un nuevo registro (POST)
     public function store(Request $request)
     {
-        //
+        $movie = new Movie();
+        $movie->name = $request->input('name');
+        $movie->description = $request->input('description');
+        $movie->year = $request->input('year');
+        $movie->genre = $request->input('genre');
+        $movie->duration = $request->input('duration');
+
+        $movie->save();
+        echo json_encode($movie);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Movie  $movie
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Movie $movie)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Movie  $movie
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Movie $movie)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
@@ -67,9 +46,17 @@ class MovieController extends Controller
      * @param  \App\Models\Movie  $movie
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Movie $movie)
+    public function update(Request $request, $movie_id)
     {
-        //
+        $movie = Movie::find($movie_id);
+        $movie->name = $request->input('name');
+        $movie->description = $request->input('description');
+        $movie->year = $request->input('year');
+        $movie->genre = $request->input('genre');
+        $movie->duration = $request->input('duration');
+
+        $movie->save();
+        echo json_encode($movie);
     }
 
     /**
@@ -78,8 +65,9 @@ class MovieController extends Controller
      * @param  \App\Models\Movie  $movie
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Movie $movie)
+    public function destroy($movie_id)
     {
-        //
+        $movie = Movie::find($movie_id);
+        $movie->delete();
     }
 }
